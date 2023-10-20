3cf9ce50af069eef3b25adb8628194f4f9b9e924
Cahir <artur08062003@mail.ru>
2023-10-20 21:45:21 +0600

    Created table User rework

--- backend/src/database/models.py
+++ backend/src/database/models.py
@@ -10,8 +10,8 @@ from sqlalchemy.orm import mapped_column
 class User(Base):
     __tablename__ = "user"
     id:Mapped[int] = mapped_column(primary_key=True,nullable=False)
-    username: Mapped[str] = mapped_column(String(64),index=True,unique=True,nullable=False)
+    username: Mapped[str] = mapped_column(String(64),index=True,nullable=False)
     email: Mapped[str] = mapped_column(String(120),nullable=False,unique=True)
-    hash_pass: Mapped[Optional[str]]= mapped_column(String(128),nullable=False,unique=True)
-    salt:Mapped[Optional[str]] = mapped_column(String(1024))
-    about_me: Mapped[Optional[str]] = mapped_column(String(140))
+    hash_pass: Mapped[str] = mapped_column(String(128),nullable=False)
+    salt:Mapped[str] = mapped_column(String(32))
+    about_me: Mapped[str] = mapped_column(String(140))

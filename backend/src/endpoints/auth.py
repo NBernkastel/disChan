9bade2dc6f59cc55ae5924823936eebb9562dd79
NBernkastel <nexeland@gmail.com>
2023-10-21 14:44:47 +0600

    add user scheme

--- backend/src/endpoints/auth.py
+++ backend/src/endpoints/auth.py
@@ -1,3 +1,11 @@
 from fastapi import APIRouter
 
+from shemes.auth import UserLogin
+
 auth_router = APIRouter(prefix='/auth', tags=['Auth'])
+
+
+
+auth_router.post('/login')
+async def login(user: UserLogin):
+    pass

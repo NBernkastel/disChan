c1225879e153359515f7b7d09e89eb9d43646048
Cahir <artur08062003@mail.ru>
2023-10-22 03:12:13 +0600

    Created repository alpha

--- backend/src/utils/repository.py
+++ backend/src/utils/repository.py
@@ -1,10 +1,48 @@
+import abc
+
 from abc import ABC, abstractmethod
 from ..database.db_config import async_session_maker
 
 
-class AbstractRepository(ABC):
-    pass
+class AbstractRepository(metaclass=abc.ABCMeta):
+
+    @abc.abstractmethod
+    def add_one(self, obj):
+        raise NotImplemented()
+
+    @abc.abstractmethod
+    def get_one(self, obj, primary_key):
+        raise NotImplemented()
+
+    @abc.abstractmethod
+    def delete_one(self, obj):
+        raise NotImplemented()
+
+    @abc.abstractmethod
+    def mark_as_delete(self, obj):
+        raise NotImplemented()
+
+    @abc.abstractmethod
+    def get_all_by_filter(self, obj, filter_condition):
+        raise NotImplemented()
 
 
 class SQLAlchemyRepository(AbstractRepository):
-    model = None
+    def __init__(self, session: async_session_maker, model_class):
+        self.session = session
+        self.model_class = model_class
+
+    def add_one(self, obj):
+        self.session.add(obj)
+
+    def delete_one(self, obj):
+        self.session.delete(obj)
+
+    def get_one(self, obj, primary_key):
+        return self.session.querry(self.model_class).get(primary_key)
+
+    def mark_as_delete(self, obj):
+        obj.is_delete = True
+
+    def get_all_by_filter(self, obj, filter_condition):
+        return self.session.querry(self.model_class).filter(filter_condition).all()
